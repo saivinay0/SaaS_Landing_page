4a09d91c06478632dcaf705c67c8e5e9e5972b38
sai vinay <saiv71155@gmail.com>
2025-01-25 20:35:25 +0530

    Add Pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Features from "./section/Features.jsx";
 import Header from "./section/Header.jsx";
 import Hero from "./section/Hero.jsx";
+import Pricing from "./section/Pricing.jsx";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 };
