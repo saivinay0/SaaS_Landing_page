b09f7f0515bb690868ec54312418e2381d130c77
sai vinay <saiv71155@gmail.com>
2025-01-25 16:44:56 +0530

    Create Header,Hero,Features section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Features from "./section/Features.jsx";
+import Header from "./section/Header.jsx";
+import Hero from "./section/Hero.jsx";
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
+};
+
+export default App;
